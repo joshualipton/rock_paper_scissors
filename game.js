7683642828e8f962e80b0610f393499e1ecbd3bc
joshualipton <jalipton11@gmail.com>
2022-08-05 21:15:02 -0700

    Add score

--- game.js
+++ game.js
@@ -34,36 +34,43 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function updateScore(playerValue, computerValue) {
+  const playerScore = document.querySelector(".player-score");
+  playerScore.textContent = `Player: ${playerValue}`;
+
+  const computerScore = document.querySelector(".computer-score");
+  computerScore.textContent = `Computer: ${computerValue}`;
+}
+
 function game() {
   let playerScore = 0;
   let computerScore = 0;
 
-  const buttons = document.querySelector("selector-button");
-
-  let playerChoice = "";
+  const buttons = document.querySelectorAll(".selector-button");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      playerChoice = button.id;
+      let roundResult = playRound(button.id, getComputerChoice());
+      if (roundResult.charAt(4) === "W") {
+        ++playerScore;
+        console.log("Win");
+      } else if (roundResult.charAt(4) === "L") {
+        ++computerScore;
+        console.log("lose");
+      }
+      updateScore(playerScore, computerScore);
+
+      if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore) {
+          console.log("You Win!");
+        } else if (computerScore > playerScore) {
+          console.log("You Lose!");
+        } else {
+          console.log("Tie!");
+        }
+      }
     });
   });
-
-  console.log(playerChoice);
-
-  let roundResult = playRound(playerChoice, getComputerChoice());
-  console.log(roundResult);
-
-  if (roundResult.charAt(4) === "W") {
-    ++playerScore;
-  } else if (roundResult.charAt(4) === "L") {
-    ++computerScore;
-  }
-
-  if (playerScore > computerScore) {
-    console.log("You Win!");
-  } else if (computerScore > playerScore) {
-    console.log("You Lose!");
-  } else {
-    console.log("Tie!");
-  }
 }
+
+game();
